13c862d790c62c649245506cb49a8550ca4fea4f
takbirgazi <takbirgazibd@gmail.com>
2024-06-09 02:43:04 +0600

    Require JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config()
